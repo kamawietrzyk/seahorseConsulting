fe7a907e884a4d3c4b02fc24792c64543444411a
Kama <kama@wietrzyk.com>
2020-09-29 13:53:36 +0200

    ScrollToTop added + minor style corrections

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,11 @@ import Blog from './Blog';
 import Company from './Company';
 import Contact from './Contact';
 import References from './References';
+import ScrollToTop from './ScrollToTop';
 
 const Home = () => (
     <>
+        <ScrollToTop />
         <Company />
         <About />
         <Blog />
