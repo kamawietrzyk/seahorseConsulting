c25ab971a6580f5d684b33fec33a3771dd7bcb38
Kama <kama@wietrzyk.com>
2020-09-30 19:33:23 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import URLS from './utils/urls';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/Home';
 import PrivacyPolicy from './components/PrivacyPolicy';
 
